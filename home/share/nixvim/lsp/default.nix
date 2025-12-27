208fe1e14e6356bdffe1db5a5dd8c3829f4916a8
frofor <18nraywczifc@protonmail.com>
2025-12-27 13:37:08 +0100

    feat(nixvim): add python lsp

--- home/share/nixvim/lsp/default.nix
+++ home/share/nixvim/lsp/default.nix
@@ -4,6 +4,7 @@
   imports = [
     ./haskell.nix
     (import ./nix.nix { inherit host; })
+    ./python.nix
     ./rust.nix
     ./zig.nix
   ];
---
+++ home/share/nixvim/lsp/python.nix
@@ -0,0 +1,3 @@
+{
+  programs.nixvim.plugins.lsp.servers.pylyzer.enable = true;
+}
