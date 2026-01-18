a661b1e6fac107af31188d003d87a22117f6f256
frofor <18nraywczifc@protonmail.com>
2026-01-18 15:23:04 +0100

    fix(nvim): autocmd to jump to last position

--- home/share/nixvim/autocmds.nix
+++ home/share/nixvim/autocmds.nix
@@ -3,8 +3,10 @@
     event = "BufReadPost";
     callback.__raw = ''
       function()
-        local line = vim.fn.line([[\"]])
-        if line > 0 and line <= vim.fn.line('$') then
+        local line = vim.api.nvim_buf_get_mark(0, '"')[1]
+        local last_line = vim.api.nvim_buf_line_count(0)
+
+        if line > 1 and line <= last_line then
             vim.cmd('normal! g`"')
         end
       end
