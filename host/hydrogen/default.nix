5bd573a26aa819624f2cca15f1f0f86ca1921eb4
frofor <18nraywczifc@protonmail.com>
2026-01-18 15:46:36 +0100

    feat: remove unused vpns

--- host/hydrogen/default.nix
+++ host/hydrogen/default.nix
@@ -57,56 +57,9 @@
       ];
       encryption.mode = "none";
     };
-    openvpn.servers = {
-      canada = {
-        config = "config \"/home/${user}/documents/vpn/canada.ovpn";
-        updateResolvConf = true;
-        autoStart = false;
-      };
-      japan = {
-        config = "config \"/home/${user}/documents/vpn/japan.ovpn";
-        updateResolvConf = true;
-        autoStart = false;
-      };
-      mexico = {
-        config = "config \"/home/${user}/documents/vpn/mexico.ovpn";
-        updateResolvConf = true;
-        autoStart = false;
-      };
-      netherlands = {
-        config = "config \"/home/${user}/documents/vpn/netherlands.ovpn";
-        updateResolvConf = true;
-        autoStart = false;
-      };
-      norway = {
-        config = "config \"/home/${user}/documents/vpn/norway.ovpn\"";
-        updateResolvConf = true;
-        autoStart = false;
-      };
-      poland = {
-        config = "config \"/home/${user}/documents/vpn/poland.ovpn\"";
-        updateResolvConf = true;
-        autoStart = false;
-      };
-      romania = {
-        config = "config \"/home/${user}/documents/vpn/romania.ovpn\"";
-        updateResolvConf = true;
-        autoStart = false;
-      };
-      singapore = {
-        config = "config \"/home/${user}/documents/vpn/singapore.ovpn\"";
-        updateResolvConf = true;
-        autoStart = false;
-      };
-      switzerland = {
-        config = "config \"/home/${user}/documents/vpn/switzerland.ovpn\"";
-        updateResolvConf = true;
-        autoStart = false;
-      };
-      united-states = {
-        config = "config \"/home/${user}/documents/vpn/united-states.ovpn\"";
-        updateResolvConf = true;
-      };
+    openvpn.servers.united-states = {
+      config = "config \"/home/${user}/documents/vpn/united-states.ovpn\"";
+      updateResolvConf = true;
     };
   };
   programs.zsh.enable = true;
