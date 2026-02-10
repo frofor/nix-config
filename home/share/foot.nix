78c227df7f838eb4099a0512c289680ff181c665
frofor <18nraywczifc@protonmail.com>
2026-02-10 19:35:23 +0100

    feat(foot): scroll with `Alt`+`d`/`u`

--- home/share/foot.nix
+++ home/share/foot.nix
@@ -4,6 +4,12 @@
   programs.foot = {
     enable = true;
     server.enable = true;
-    settings.main.font = "${fontMono}:size=14";
+    settings = {
+      main.font = "${fontMono}:size=14";
+      "key-bindings" = {
+        "scrollback-down-half-page" = "Alt+d";
+        "scrollback-up-half-page" = "Alt+u";
+      };
+    };
   };
 }
