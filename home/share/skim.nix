fc9f4908b4a6ca4edc8e57a1d6fde301647fb2b0
frofor <18nraywczifc@protonmail.com>
2026-02-14 22:28:34 +0100

    feat: skim config

--- home/share/skim.nix
+++ home/share/skim.nix
@@ -2,5 +2,9 @@
   programs.skim = {
     enable = true;
     enableZshIntegration = true;
+    defaultOptions = [
+      "--no-mouse"
+      "--color=none,matched:#ffff00,current:#00ffff,cursor:#00ffff"
+    ];
   };
 }
