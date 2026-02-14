251a6c59179ec975183e6fa81b120f5470d0448d
frofor <18nraywczifc@protonmail.com>
2026-02-15 00:09:47 +0100

    feat(scripts): error handling

--- home/share/scripts.nix
+++ home/share/scripts.nix
@@ -13,17 +13,27 @@ in
       text = ''
         #!/bin/sh
         attrs=$(sed 1d)
-        key=$(printf "$attrs" | awk -F : '{print $1}' | sk)
-        wl-copy $(printf "$attrs" | grep "^$key:" | sed 's/^[^:]*: *//')
-        echo "Copied \033[33m$key\033[0m to clipboard."
+        attr=$(printf '%s' "$attrs" | awk -F : '{print $1}' | sk -p 'Choose an attribute: ')
+        [ -z "$attr" ] && exit 1
+
+        wl-copy $(printf '%s' "$attrs" | grep "^$attr:" | sed 's/^[^:]*: *//')
+        echo "Copied \033[33m$attr\033[0m to clipboard"
+        notify-send "Copied $attr to clipboard"
       '';
       executable = true;
     };
     "${scriptsDir}/screenshot.sh" = {
       text = ''
         #!/bin/sh
+        sel="$(${slurpBin})"
+        [ "$?" -ne 0 ] && exit 1
+
         mkdir -p '${screenshotsDir}'
-        ${grimBin} -g "$(${slurpBin})" "${screenshotsDir}/$(date +%Y-%m-%d-%H-%M-%S).png"
+        dst="${screenshotsDir}/$(date +%Y-%m-%d-%H-%M-%S).png"
+        ${grimBin} -g "$sel" "$dst"
+
+        echo "Screenshot saved to \033[33m'$dst'\033[0m"
+        notify-send "Screenshot saved to '$dst'"
       '';
       executable = true;
     };
@@ -31,7 +41,8 @@ in
       text = ''
         #!/bin/sh
         svs=$(systemctl list-units -t service --plain --no-legend | awk '{print $1}')
-        sv=$(printf "%s\n" "$svs" | sk -p 'Choose a service: ')
+        sv=$(printf '%s' "$svs" | sk -p 'Choose a service: ')
+        [ -z "$sv" ] && exit 1
         journalctl -feu "$sv"
       '';
       executable = true;
@@ -40,7 +51,11 @@ in
       text = ''
         #!/bin/sh
         apps=$(fd -e desktop . '/etc/profiles/per-user/${user}/share/applications')
-        app=$(printf "%s\n" "$apps" | sk -p 'Choose an application: ')
+        app=$(printf '%s' "$apps" | sk -p 'Choose an application: ')
+        [ -z "$app" ] && exit 1
+
+        echo "Launching \033[33m$app\033[0m..."
+        notify-send "Launching $app..."
         swaymsg exec "${dexBin} '$app'"
       '';
       executable = true;
@@ -48,18 +63,32 @@ in
     "${scriptsDir}/sk-service.sh" = {
       text = ''
         #!/bin/sh
-        action=$(printf 'status\nstart\nstop\nrestart\n' | sk -p 'Choose an action: ')
-        case "$action" in
-            status) flags=--all ;;
-            start|restart) flags='--state loaded,inactive,failed' ;;
+        act=$(printf 'status\nstart\nstop\nrestart' | sk -p 'Choose an action: ')
+        case "$act" in
+            ''') exit 1 ;;
+            status) flags=' --all' ;;
+            start|restart) flags=' --state loaded,inactive,failed' ;;
         esac
 
-        sys=$(systemctl list-units -t service $flags --plain --no-legend | awk '{print $1}')
-        usr=$(systemctl list-units -t service $flags --user --plain --no-legend | awk '{print $1}')
-        sv=$(printf "%s\n%s\n" "$sys" "$usr" | sk -p 'Choose a service: ')
+        cmd="systemctl list-units -t service$flags --plain --no-legend"
+        sys=$($cmd | awk '{print "[sys] " $1}')
+        usr=$($cmd --user | awk '{print "[usr] " $1}')
 
-        echo "$usr" | grep -q "^$sv$" && flags=--user || flags='''
-        systemctl "$action" $flags "$sv" | less
+        sv=$(printf '%s%s' "$sys" "$usr" | sk -p 'Choose a service: ')
+        case "$sv" in
+            ''') exit 1 ;;
+            [sys]*) flags=' --user' ;;
+            *) flags=''' ;;
+        esac
+
+        cmd="systemctl $act$flags $(printf '%s' "$sv" | sed 's/^\[.*\] //')"
+        if [ "$act" = status ]; then
+            $cmd | less
+        else
+            echo "Executing \`$cmd\`..."
+            notify-send "Executing \`$cmd\`..."
+            $cmd
+        fi
       '';
       executable = true;
     };
