f775a2eeb010f5c746b9a16d50b8e527ac0ec44a
frofor <18nraywczifc@protonmail.com>
2026-02-17 23:00:46 +0100

    feat(mako): set `width` to 500

--- home/share/mako.nix
+++ home/share/mako.nix
@@ -6,6 +6,7 @@
     settings = {
       inherit font;
       background-color = "#000000";
+      width = 500;
       border-size = 1;
       border-color = "#00ffff";
       default-timeout = 10000;
