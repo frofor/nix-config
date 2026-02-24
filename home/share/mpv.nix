c46b1ace5bdc6efac4e3cd33dc5193340f000ef1
frofor <18nraywczifc@protonmail.com>
2026-02-24 21:40:48 +0100

    feat(mpv): save watch history

--- home/share/mpv.nix
+++ home/share/mpv.nix
@@ -3,7 +3,10 @@
 {
   programs.mpv = {
     enable = true;
-    config.shuffle = "yes";
+    config = {
+      shuffle = "yes";
+      save-watch-history = "yes";
+    };
     scripts = with pkgs.mpvScripts; [ mpris mpv-notify-send ];
   };
 }
