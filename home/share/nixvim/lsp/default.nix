0edac2c38fe3e0f26875110ffac5df969b207d72
frofor <18nraywczifc@protonmail.com>
2026-03-04 18:23:10 +0100

    feat(nixvim): lean lsp

--- home/share/nixvim/lsp/default.nix
+++ home/share/nixvim/lsp/default.nix
@@ -1,4 +1,4 @@
 {
-  imports = [ ./haskell.nix ./nix.nix ./python.nix ./rust.nix ./zig.nix ];
+  imports = [ ./haskell.nix ./lean.nix ./nix.nix ./python.nix ./rust.nix ./zig.nix ];
   programs.nixvim.plugins.lsp.enable = true;
 }
---
+++ home/share/nixvim/lsp/lean.nix
@@ -0,0 +1,3 @@
+{
+  programs.nixvim.plugins.lean.enable = true;
+}
