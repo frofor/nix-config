87aeac5c6560656e7772f15fa9ef58ed0fa9b015
frofor <18nraywczifc@protonmail.com>
2026-03-08 10:15:34 +0100

    fix(scripts): `pass` piped into `doas` instead of `cryptsetup`

--- lib/scripts/luks-open.nix
+++ lib/scripts/luks-open.nix
@@ -2,9 +2,11 @@
 
 pkgs.writeShellScriptBin "luks-open" ''
   #!/bin/sh
-  printf %s "$(pass '${passEntry}')" \
+  pass=$(pass '${passEntry}') || exit 1
+  printf %s "$pass" \
       | doas cryptsetup open --key-file=- /dev/disk/by-uuid/${uuid} luks-${uuid} \
-      && doas mkdir -p /mnt/${uuid} \
-      && doas mount /dev/mapper/luks-${uuid} /mnt/${uuid} \
-      && ${pkgs.libnotify}/bin/notify-send '${uuid} opened and mounted'
+      || exit 1
+  doas mkdir -p /mnt/${uuid}
+  doas mount /dev/mapper/luks-${uuid} /mnt/${uuid} || exit 1
+  ${pkgs.libnotify}/bin/notify-send '${uuid} opened and mounted'
 ''
