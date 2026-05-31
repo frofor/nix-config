4b7a83da41fa5f75fa07a5ca6077afbee14883ab
frofor <18nraywczifc@protonmail.com>
2026-05-31 17:56:01 +0200

    feat(nixvim): java lsp

--- home/nixvim/lsp/default.nix
+++ home/nixvim/lsp/default.nix
@@ -1,4 +1,4 @@
 {
-  imports = [ ./haskell.nix ./lean.nix ./nix.nix ./python.nix ./rust.nix ./zig.nix ];
+  imports = [ ./haskell.nix ./java.nix ./lean.nix ./nix.nix ./python.nix ./rust.nix ./zig.nix ];
   programs.nixvim.plugins.lsp.enable = true;
 }
---
+++ home/nixvim/lsp/java.nix
@@ -0,0 +1,3 @@
+{
+  programs.nixvim.plugins.lsp.servers.jdtls.enable = true;
+}
