b3f218df17fab6d764635465b5939a60c6b00755
frofor <18nraywczifc@protonmail.com>
2026-06-26 15:31:24 +0200

    xdg: use `xdg-user-dir` instead of `$XDG_*_DIR`

--- lib/scripts/sk-cd.nix
+++ lib/scripts/sk-cd.nix
@@ -2,7 +2,7 @@
 
 pkgs.writeShellScript "sk-cd.sh" ''
   #!/bin/sh
-  prj=$(find "$XDG_DOCUMENTS_DIR/projects" -mindepth 1 -maxdepth 1 -type d)
+  prj=$(find "$(xdg-user-dir DOCUMENTS)/projects" -mindepth 1 -maxdepth 1 -type d)
   src=$(find "$HOME/.local/src" -mindepth 1 -maxdepth 1 -type d)
   dir=$(printf '%s\n%s\n' "$prj" "$src" | sed /^$/d | sort | sk -p 'Choose a directory: ') || exit 1
   swaymsg "exec footclient -D '$dir'"
--- lib/scripts/sk-playlist.nix
+++ lib/scripts/sk-playlist.nix
@@ -2,6 +2,6 @@
 
 pkgs.writeShellScript "sk-playlist.sh" ''
   #!/bin/sh
-  playlist=$(find "$XDG_MUSIC_DIR" -name '*.m3u' | sk -p 'Choose a playlist: ') || exit 1
+  playlist=$(find "$(xdg-user-dir MUSIC)" -name '*.m3u' | sk -p 'Choose a playlist: ') || exit 1
   swaymsg exec "umpv '$playlist'"
 ''
